b513e90d9f1ddabbfe637cf0afb8bc2f9f9300de
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-09 15:50:02 +0330

    update with ScrollToTop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "../styles/fonts.css";
 import Navbar from "./components/ui/Navbar";
+import ScrollToTop from "./components/ui/ScrollToTop";
 
 export const metadata: Metadata = {
   title: "Flower Shop",
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body>
         <Navbar />
         {children}
+        <ScrollToTop />
       </body>
     </html>
   );
