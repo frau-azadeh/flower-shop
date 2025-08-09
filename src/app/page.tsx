6df8a0caf181badc7941ff8497f7c8e5ba437fdf
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-09 10:43:02 +0330

    test color and background in project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1 className="font-bold text-red-500 text-2xl">فروشگاه گل </h1>
+    <div className="bg-background text-text font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <h1 className="text-accent font-bold text-2xl">فروشگاه گل</h1>
+       <h1 className="text-primary font-bold text-2xl">به فروشگاه گل من خوش آمدید</h1> 
     </div>
   );
 }
