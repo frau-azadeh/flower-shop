79f4fbc368998c6bc84dc6e9d4971a3ad174a87e
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-09 22:29:37 +0330

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import WhyUs from "./components/WhyUs";
 
+
 export default function Home() {
   return (
     <div className="bg-background flex flex-col items-center justify-items-center min-h-screen ">
       <h1 className="text-accent font-bold text-2xl m-10">گل فروش</h1>
 
       <WhyUs />
+
     </div>
   );
 }
