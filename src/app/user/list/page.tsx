9ecc7b780a570e34195a5f71dd5c3123eb94379b
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-10 17:23:34 +0330

    add page list

---
+++ src/app/user/list/page.tsx
@@ -0,0 +1,12 @@
+import Wishlists from '@/app/components/user/WishLists'
+import React from 'react'
+
+const UserList = () => {
+  return (
+<div>
+    <Wishlists/>
+</div>
+)
+}
+
+export default UserList
