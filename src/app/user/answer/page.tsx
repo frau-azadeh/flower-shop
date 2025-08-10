3d5933d3c9250de6332fc308ddc89c18a4daf3b8
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-10 17:23:14 +0330

    add answer page

---
+++ src/app/user/answer/page.tsx
@@ -0,0 +1,12 @@
+import Answer from '@/app/components/user/Answer'
+import React from 'react'
+
+const AnswerUser:React.FC = () => {
+  return (
+    <div>
+        <Answer/>
+    </div>
+  )
+}
+
+export default AnswerUser
