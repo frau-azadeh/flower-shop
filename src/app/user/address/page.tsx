6e75468f2b1798a014be9758df1e19dc76c2cc94
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-10 17:23:02 +0330

    add adress page

---
+++ src/app/user/address/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import UserAddress from '@/app/components/user/UserAddress'
+const Adress:React.FC = () => {
+  return (
+    <div>
+       <UserAddress/>
+    </div>
+  )
+}
+
+export default Adress
