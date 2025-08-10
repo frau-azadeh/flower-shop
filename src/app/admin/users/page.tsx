f8e97c89732e53309021b363c70cf2e831bb5d56
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-10 22:57:18 +0330

    add page users

---
+++ src/app/admin/users/page.tsx
@@ -0,0 +1,9 @@
+import UserAccessCard from "@/app/components/admin/UsersAccessCard";
+
+export default function AddUsers(){
+    return(
+        <div>
+            <UserAccessCard/>
+        </div>
+    )
+}
