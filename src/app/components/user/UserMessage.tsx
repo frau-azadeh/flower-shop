9d70bf195d0fca90975a588344cec4353a0affdd
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-11 16:09:40 +0330

    add use client and change text to emptyText because to show message

--- src/app/components/user/UserMessage.tsx
+++ src/app/components/user/UserMessage.tsx
@@ -1,38 +1,45 @@
-"use client";
-import { Archive, Inbox, Mail, Star } from "lucide-react";
-import Tabs, { TabItem } from "@/app/components/ui/Tabs";
+"use client"
+import React from 'react'
+import Tabs, { TabItem } from '../ui/Tabs'
+import { Archive, Inbox, Mail, Star } from 'lucide-react'
 
-const messageTabs: TabItem[] = [
+const messageTabs: TabItem[] =[
   {
-    key: "inbox",
-    label: "همه پیامها",
-    icon: <Inbox className="size-4" />,
-    emptyIcon: <Inbox className="size-24 text-slate-300" />,
-    text: "هنوز پیامی در صندوق شما نیست",
+    key:"inbox",
+    label:"همه پیامها",
+    icon:<Inbox className='size-4'/>,
+    emptyIcon:<Inbox className='size-24 text-slate-300'/>,
+    emptyText: "هنوز پیامی در صندوق شما نیست",
   },
   {
     key: "unread",
     label: "خوانده نشده",
-    icon: <Mail className="size-4" />,
-    emptyIcon: <Mail className="size-24 text-slate-300" />,
-    text: "پیام خوانده نشده ای ندارید",
+    icon: <Mail className='size-4'/>,
+    emptyIcon: <Mail className='size-24 text-slate-300'/>,
+    emptyText: "پیام خوانده نشده ای ندارید",
   },
   {
     key: "starred",
     label: "ستاره دار",
-    icon: <Star className="size-4" />,
-    emptyIcon: <Star className="size-24 text-slate-300" />,
-    text: "پیام ستاره دار ثبت نشده است",
+    icon: <Star className='size-4'/>,
+    emptyIcon:<Star className='size-24 text-slate-300'/>,
+    emptyText:"پیامی در بایگانی وجود ندارد",
   },
   {
     key: "archive",
-    label: "بایگانی",
-    icon: <Archive className="size-4" />,
-    emptyIcon: <Archive className="size-24 text-slate-300" />,
-    text: "پیامی در بایگانی وجود ندارد",
+    label:"بایگانی",
+    icon:<Archive className='size-4'/>,
+    emptyIcon: <Archive className='size-24 text-slate-300'/>,
+    emptyText: "پیامی در بایگانی وجود ندارد",
   },
 ];
 
-export default function UserMessage() {
-  return <Tabs tabs={messageTabs} />;
+const UserMessage:React.FC = () => {
+  return (
+    <div>
+      <Tabs tabs={messageTabs}/>
+    </div>
+  )
 }
+
+export default UserMessage
--- src/app/components/user/WishLists.tsx
+++ src/app/components/user/WishLists.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from "react";
 import Tabs, { TabItem } from "../ui/Tabs";
 import { Bell, Heart, Info, List, ToggleRight } from "lucide-react";
