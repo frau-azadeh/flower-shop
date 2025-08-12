e439e568eb78d898726e8bc5b44b825020448e87
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-12 20:58:49 +0330

    add auth/login

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { loginSchema, type LoginSchema } from "@/schemas/auth.schema";
+import Input from "@/app/components/ui/Input";
+import Button from "@/app/components/ui/Button";
+import { createSupabaseClient } from "@/lib/supabase";
+import {  Mail } from "lucide-react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { showSuccess, showError } from "@/lib/toast";
+
+export default function LoginPage() {
+  const supabase = createSupabaseClient();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+    mode: "onChange",
+  });
+
+  const router = useRouter();
+
+  const onSubmit = async (data: LoginSchema) => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email: data.email,
+      password: data.password,
+    });
+
+    if (error) {
+      showError("ورود ناموفق بود. لطفاً ایمیل یا رمز عبور را بررسی کنید.");
+    } else {
+      showSuccess("ورود موفقیت‌آمیز بود!");
+      router.push("/user/dashboard");
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-background px-4">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="bg-white p-6 rounded-lg shadow-md w-full max-w-sm space-y-4"
+      >
+        <h2 className="text-xl font-bold text-center">ورود به حساب</h2>
+
+        <Input
+          label="ایمیل"
+          type="email"
+          icon={<Mail size={18} />}
+          {...register("email")}
+          error={errors.email?.message}
+        />
+
+        <Input
+          label="رمز عبور"
+          type="password"
+          togglePassword
+          {...register("password")}
+          error={errors.password?.message}
+        />
+
+        <Button type="submit" loading={isSubmitting} className="w-full">
+          ورود
+        </Button>
+
+        <p className="text-sm text-center text-gray-600">
+          حساب ندارید؟{" "}
+          <Link
+            href="/auth/signup"
+            className="text-blue-600 hover:underline font-medium"
+          >
+            ثبت‌نام کنید
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+}
