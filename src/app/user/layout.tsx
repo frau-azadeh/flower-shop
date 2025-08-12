6c974a03476902d5b785d99590f2e4224c88e8a5
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-12 20:58:04 +0330

    add auth route

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,12 +1,29 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { createSupabaseClient } from "@/lib/supabase";
 import ScrollToTop from "@/app/components/ui/ScrollToTop";
 import UserFooter from "@/app/components/user/UserFooter";
 import UserNavbar from "@/app/components/user/UserNavbar";
 
-export default function AdminLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  const supabase = createSupabaseClient();
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      if (!session) {
+        router.push("/auth/login");
+      } else {
+        setLoading(false);
+      }
+    });
+  }, []);
+
+  if (loading) return null; // یا یک لودر بگذار
+
   return (
     <div className="bg-background">
       <div className="min-h-screen grid grid-cols-1 md:grid-cols-[240px_1fr]">
