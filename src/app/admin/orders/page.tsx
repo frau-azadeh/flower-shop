faedc6f6f1f88707c2c8d77e6f9fb15faa4270f3
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-14 21:42:08 +0330

    update oders

--- src/app/admin/orders/page.tsx
+++ src/app/admin/orders/page.tsx
@@ -1,11 +1,15 @@
 // app/admin/orders/page.tsx
 import OrdersCard from "@/app/components/admin/OrdersCard";
 import { Suspense } from "react";
+import RequireAuth from "@/app/components/admin/RequireAuth";
+
 
 export default function OrdersAdmin() {
   return (
     <Suspense fallback={null}>
-      <OrdersCard />
+       <RequireAuth allow={["PRODUCTS", "FULL"]}>
+ <OrdersCard />
+  </RequireAuth>
     </Suspense>
   );
 }
