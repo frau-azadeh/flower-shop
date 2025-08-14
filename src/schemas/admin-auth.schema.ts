0786e893c765d0eac3dc70e4285fa2efb78873bc
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-14 21:45:51 +0330

    add schema

---
+++ src/schemas/admin-auth.schema.ts
@@ -0,0 +1,16 @@
+import z from "zod";
+
+export const adminLoginSchema = z.object({
+    firstName: z
+        .string()
+        .min(2,"نام کوتاه است"),
+    lastName: z
+        .string()
+        .max(15,"تعداد کاراکترها مجاز نیست")
+        .min(2,"نام خانوادگی کوتاه است"),
+    password: z
+        .string()
+        .min(6,"حداقل 6 کاراکتر مجاز ")
+});
+
+export type AdminLoginSchema = z.infer<typeof adminLoginSchema>;
