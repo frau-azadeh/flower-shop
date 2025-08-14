65d7a33c31a8a16ba8abf7de5589dc8718b39978
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-14 21:40:59 +0330

    add blog page

---
+++ src/app/admin/blog/page.tsx
@@ -0,0 +1,14 @@
+import PostEditor from "@/app/components/admin/PostEditor";
+import RequireAuth from "@/app/components/admin/RequireAuth";
+
+export default function Page() {
+  return (
+    <RequireAuth allow={["BLOG", "FULL"]}>
+      <PostEditor />
+    </RequireAuth>
+  );
+}
+
+
+
+
