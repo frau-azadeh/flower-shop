30a9139d5e9d8142a8d5e97847e614b2e318d3cb
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-16 20:28:29 +0330

    add type

---
+++ src/types/blog.ts
@@ -0,0 +1,24 @@
+export type Role = "BLOG" | "PRODUCTS" | "FULL";
+export type Status = "draft" | "published";
+
+export interface AdminMinimal {
+  role: Role;
+  isActive: boolean;
+}
+
+export interface SavePayload {
+  title: string;
+  slug: string;
+  content?: string;
+  status?: Status;
+  coverUrl?: string; // اگر در DB NOT NULL است، ما خالی می‌فرستیم
+}
+
+export interface PublishPayload {
+  slug: string;
+}
+
+export interface IdSlug {
+  id: string;
+  slug: string;
+}
