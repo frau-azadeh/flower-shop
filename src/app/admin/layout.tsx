b6287627bbbc9d35c53ff6fbc9839e5b5a0347a9
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-21 22:02:39 +0330

    update

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,41 +1,19 @@
-"use client";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { createSupabaseClient } from "@/lib/supabase";
+import AdminHeader from "@/app/components/admin/AdminHeader";
+import AdminFooter from "@/app/components/admin/AdminFooter";
 import ScrollToTop from "@/app/components/ui/ScrollToTop";
-import UserFooter from "@/app/components/user/UserFooter";
-import UserNavbar from "@/app/components/user/UserNavbar";
-
-export default function UserLayout({
+export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [loading, setLoading] = useState(true);
-  const supabase = createSupabaseClient();
-  const router = useRouter();
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      if (!session) {
-        const current = `${window.location.pathname}${window.location.search}`;
-        router.push(`/auth/login?redirect=${encodeURIComponent(current)}`); // ⬅️ مسیر فعلی حفظ شود
-      } else {
-        setLoading(false);
-      }
-    });
-  }, [router, supabase.auth]);
-
-  if (loading) return null;
-
   return (
     <div className="bg-background">
-      <div className="min-h-screen grid grid-cols-1 lg:grid-cols-[280px_1fr]">
-        <UserNavbar />
+      <div className="min-h-screen grid grid-cols-1 md:grid-cols-[240px_1fr]">
+        <AdminHeader />
         <main className="p-4 md:p-6">{children}</main>
       </div>
       <ScrollToTop />
-      <UserFooter />
+      <AdminFooter />
     </div>
   );
 }
