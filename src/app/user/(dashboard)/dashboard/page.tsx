e916d5cb64f448850077dae9b56957fe8cb12ecb
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-23 10:24:42 +0330

    update and refactore useSearchParams error

--- src/app/user/(dashboard)/dashboard/page.tsx
+++ src/app/user/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import OrderHistory from "@/app/components/user/OrderHistory";
 import AutoCheckout from "@/app/components/user/AutoCheckout";
 import { Suspense } from "react";
+export const dynamic = "force-dynamic"; // یا: export const revalidate = 0;
+
 export default function UserDashboard() {
   return (
     <Suspense fallback={null}>
