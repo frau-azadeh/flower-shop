e3fa4440136713100be44b3a7bf892b3c02a8181
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-23 10:57:35 +0330

    add

---
+++ src/app/blog/components/EmptyState.tsx
@@ -0,0 +1,7 @@
+export default function EmptyState() {
+  return (
+    <div className="rounded-2xl border border-dashed border-slate-300 bg-white p-10 text-center text-slate-500">
+      موردی مطابق جستجو پیدا نشد.
+    </div>
+  );
+}
